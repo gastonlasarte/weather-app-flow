d31cad8eb25924a2cbdf33324db18fd726b3de74
Gaston Lasarte <lasarte@strands.com>
2018-09-02 18:37:25 -0300

    Started the app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Hello!</p>
+				<h1>Ah</h1>
+			</div>
+		);
+	}
+};
+
+export default App;
