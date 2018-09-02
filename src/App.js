56c84919d80783eeae6cd0615ad67d41933ecbc8
Gaston Lasarte <lasarte@strands.com>
2018-09-02 19:28:05 -0300

    Made the form work in order to modify the api call

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ const API_KEY = "9a752a1675b0c6a3efb3097603ef2b05";
 class App extends React.Component {
 	getWeather = async (e) => {
 		e.preventDefault();
-		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+		const city = e.target.elements.city.value;
+		const country = e.target.elements.country.value;
+
+		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
 		const data = await api_call.json();
 		console.log(data);
 	}
