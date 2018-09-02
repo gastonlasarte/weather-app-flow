917f2258a22479f1770784ffb13d9cc89bc3e77c
Gaston Lasarte <lasarte@strands.com>
2018-09-02 19:12:04 -0300

    Added the openweather api key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "9a752a1675b0c6a3efb3097603ef2b05";
+
 class App extends React.Component {
+	getWeather = async () => {
+		const api_call = await fetch(`http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+		const data = await api_call.json();
+	}
 	render() {
 		return (
 			<div>
