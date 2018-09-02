09e03b8aebf6388606de4fa2b98beb349895f301
Gaston Lasarte <lasarte@strands.com>
2018-09-02 20:22:36 -0300

    Changed weather class to a constant in order to improve code

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-class Weather extends React.Component {
-	render () {
-		return (
-			<div>
-				{ this.props.temperature && <p>Temperatura: { this.props.temperature }</p> }
-				{ this.props.city && <p>Ciudad: { this.props.city }</p> }
-				{ this.props.country && <p>Pais: { this.props.country }</p> }
-				{ this.props.description && <p>Condicion: { this.props.description }</p> }
-				{ this.props.error && <p>{ this.props.error }</p> }
-			</div>
-		);
-	}
-};
+const Weather = (props) => {
+	return (
+		<div>
+			{ props.temperature && <p>Temperatura: { props.temperature }</p> }
+			{ props.city && <p>Ciudad: { props.city }</p> }
+			{ props.country && <p>Pais: { props.country }</p> }
+			{ props.description && <p>Condicion: { props.description }</p> }
+			{ props.error && <p>{ props.error }</p> }
+		</div>
+	);
+}
 
 export default Weather;
